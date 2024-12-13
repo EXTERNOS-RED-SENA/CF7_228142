23a7440f993b6e2c274207ca5166baedfdc8a44f
flizcanor <flizcanor@icloud.com>
2024-12-13 04:18:37 -0500

    vf

--- src/config/global.js
+++ src/config/global.js
@@ -160,7 +160,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
+        download: 'downloads/228142_CF07_DU_V2.pdf',
       },
       {
         icono: 'fas fa-download',
